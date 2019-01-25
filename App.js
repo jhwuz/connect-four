2ce93bedf05d75fe0349f51f6bc557b0b192d5f2
jhwuz <jeremyhaowu@gmail.com>
2019-01-25 10:45:58 -0700

    cleaned up code

--- App.js
+++ App.js
@@ -2,120 +2,111 @@ import React, {Component} from 'react';
 import {Alert, Button, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
-type Props = {};
+const SQUARE_RED = 1;
+const SQUARE_YELLOW = 2;
 
-export default class App extends Component<Props> {
+export default class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this)
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
-      board: [
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0],
-          [0, 0, 0, 0, 0, 0, 0]
-      ],
+      board: this.generateBoard(),
       redTurn: true,
       redScore: 0,
       yellowScore: 0,
-      turnCount : 0,
-      gameWon : false
-  }}
+      turnCount: 0,
+      gameWon: false
+    }
+  }
 
-  reset(){
+  generateBoard () {
+    return new Array(6).fill(null).map(() => new Array(7).fill(0));
+  }
+
+  reset() {
     this.setState({
-      board : [
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0]],
-      redTurn : true,
-      gameWon : false
+      board: this.generateBoard(),
+      redTurn: true,
+      gameWon: false
     })
   }
 
-  handleClick(i, j){
-    if (this.state.gameWon){
+  handleClick(i, j) {
+    if (this.state.gameWon) {
       return;
     }
 
-    let board  = this.state.board
-    var row = i
-    const col = j
-    if (row < 5){ //find the lowest position without a disk and slide down
-        while(board[row+1][col] !== 1 && board[row+1][col] !== 2) { //iterate through to the lowest open square
-          row++
-          if (row == 5){
-            break
-          }
+    let board = this.state.board;
+    let row = i;
+    const col = j;
+    if (row < 5) { //find the lowest position without a disk and slide down
+      while (board[row + 1][col] !== 1 && board[row + 1][col] !== 2) { //iterate through to the lowest open square
+        row++;
+        if (row == 5) {
+          break
         }
+      }
     }
 
-    if (this.state.board[row][col] === 1 || this.state.board[row][col] === 2){ //don't allow a move to be overwritten
+    if (this.state.board[row][col] === 1 || this.state.board[row][col] === 2) { //don't allow a move to be overwritten
       return;
     }
 
-    let flipTurn = this.state.redTurn
-    let turnCount = this.state.turnCount
+    let flipTurn = this.state.redTurn;
+    let turnCount = this.state.turnCount;
     if (flipTurn) {
       board[row][col] = 1
-    }
-    else{
+    } else {
       board[row][col] = 2
     }
     this.setState({
-      board : board,
-      redTurn : !flipTurn,
-      turnCount : turnCount + 1
+      board: board,
+      redTurn: !flipTurn,
+      turnCount: turnCount + 1
     })
     this.checkWinner(row, col)
   }
 
-  checkWinner(i, j){
+  checkWinner(i, j) {
     const board = this.state.board
     const currPlayer = board[i][j]
     const redScore = this.state.redScore
     const yellowScore = this.state.yellowScore
 
-    if (i < 5){ //if possible, traverse down
-      let tempRow = i
+    if (i < 5) { //if possible, traverse down
+      let tempRow = i;
 
-      while(board[tempRow+1][j] === currPlayer) {
-        tempRow++
-        if (tempRow === 5){ //don't go past last element
+      while (board[tempRow + 1][j] === currPlayer) {
+        tempRow++;
+        if (tempRow === 5) { //don't go past last element
           break
         }
       }
-      if (tempRow > 2){ // there must be at least 4 in a row to win
-        if (board[tempRow][j] === currPlayer && board[tempRow-1][j] === currPlayer && board[tempRow-2][j] === currPlayer
-            && board[tempRow-3][j] === currPlayer){
+      if (tempRow > 2) { // there must be at least 4 in a row to win
+        if (board[tempRow][j] === currPlayer && board[tempRow - 1][j] === currPlayer && board[tempRow - 2][j] === currPlayer
+          && board[tempRow - 3][j] === currPlayer) {
           if (currPlayer === 1) {
             this.setState({
-              redScore : redScore + 1,
-              gameWon : true
+              redScore: redScore + 1,
+              gameWon: true
             })
             Alert.alert('Winner', `Player ${currPlayer} has won!`)
-          }
-          else if (currPlayer === 2){
+          } else if (currPlayer === 2) {
             this.setState({
-              yellowScore : yellowScore + 1,
-              gameWon : true
-            })
+              yellowScore: yellowScore + 1,
+              gameWon: true
+            });
             Alert.alert('Winner', `Player ${currPlayer} has won!`)
           }
         }
       }
 
       if (j < 6) { //if possible, traverse to the right
-        let tempCol = j
+        let tempCol = j;
 
-        while (board[i][tempCol+1] === currPlayer) {
-          tempCol++
+        while (board[i][tempCol + 1] === currPlayer) {
+          tempCol++;
           if (tempCol === 6) { //don't go past last element
             break
           }
@@ -123,7 +114,7 @@ export default class App extends Component<Props> {
 
         if (tempRow > 2) { // there must be at least 4 in a row to win
           if (board[i][tempCol] === currPlayer && board[i][tempCol - 1] === currPlayer && board[i][tempCol - 2] === currPlayer
-              && board[i][tempCol - 3] === currPlayer) {
+            && board[i][tempCol - 3] === currPlayer) {
             if (currPlayer === 1) {
               this.setState({
                 redScore: redScore + 1,
@@ -139,10 +130,9 @@ export default class App extends Component<Props> {
             }
           }
         }
-      }
-      else{ //case for if user clicks on the last column
+      } else { //case for if user clicks on the last column
         if (board[i][j] === currPlayer && board[i][j - 1] === currPlayer && board[i][j - 2] === currPlayer
-            && board[i][j - 3] === currPlayer) {
+          && board[i][j - 3] === currPlayer) {
           if (currPlayer === 1) {
             this.setState({
               redScore: redScore + 1,
@@ -162,30 +152,28 @@ export default class App extends Component<Props> {
     }//end if
   }
 
-  square(row, column){
+  renderSquare(row, column) {
     const redOrYellow = this.state.board[row][column]
-     if (redOrYellow == 1){
-       return <TouchableHighlight onPress = {() => this.handleClick(row, column)}>
-       <View style={styles.tile}>
-       <Icon name={"circle"}  style = {styles.red}/>
-       </View>
-       </TouchableHighlight>
-     }
-     else if (redOrYellow == 2) {
-       return <TouchableHighlight onPress = {() => this.handleClick(row, column)}>
-         <View style={styles.tile}>
-           <Icon name={"circle"}  style = {styles.yellow}/>
-         </View>
-       </TouchableHighlight>
-     }
-     else{
-       return <TouchableHighlight onPress = {() => this.handleClick(row, column)}>
-         <View style={styles.tile}>
-           <Icon name={"circle"}  style = {styles.white}/>
-         </View>
-       </TouchableHighlight>
-     }
-   }
+    if (redOrYellow === SQUARE_RED) {
+      return <TouchableHighlight onPress={() => this.handleClick(row, column)}>
+        <View style={styles.tile}>
+          <Icon name={"circle"} style={styles.red}/>
+        </View>
+      </TouchableHighlight>
+    } else if (redOrYellow === SQUARE_YELLOW) {
+      return <TouchableHighlight onPress={() => this.handleClick(row, column)}>
+        <View style={styles.tile}>
+          <Icon name={"circle"} style={styles.yellow}/>
+        </View>
+      </TouchableHighlight>
+    } else {
+      return <TouchableHighlight onPress={() => this.handleClick(row, column)}>
+        <View style={styles.tile}>
+          <Icon name={"circle"} style={styles.white}/>
+        </View>
+      </TouchableHighlight>
+    }
+  }
 
 
   render() {
@@ -193,78 +181,21 @@ export default class App extends Component<Props> {
       <View style={styles.container}>
         <Text style={styles.header}> Connect Four </Text>
         {
-          this.state.board.map((row, rowIndex) =>{
+          this.state.board.map((row, rowIndex) => {
             return (
-            <View style={styles.row} key={rowIndex}>
+              <View style={styles.row} key={rowIndex}>
                 {row.map((col, colIndex) => {
-                    return this.square(rowIndex, colIndex)
+                  return this.renderSquare(rowIndex, colIndex)
                 })}
-            </View>
+              </View>
             )
           })
         }
 
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(0, 0)}*/}
-          {/*{this.square(0, 1)}*/}
-          {/*{this.square(0, 2)}*/}
-          {/*{this.square(0, 3)}*/}
-          {/*{this.square(0, 4)}*/}
-          {/*{this.square(0, 5)}*/}
-          {/*{this.square(0, 6)}*/}
-        {/*</View>*/}
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(1, 0)}*/}
-          {/*{this.square(1, 1)}*/}
-          {/*{this.square(1, 2)}*/}
-          {/*{this.square(1, 3)}*/}
-          {/*{this.square(1, 4)}*/}
-          {/*{this.square(1, 5)}*/}
-          {/*{this.square(1, 6)}*/}
-        {/*</View>*/}
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(2, 0)}*/}
-          {/*{this.square(2, 1)}*/}
-          {/*{this.square(2, 2)}*/}
-          {/*{this.square(2, 3)}*/}
-          {/*{this.square(2, 4)}*/}
-          {/*{this.square(2, 5)}*/}
-          {/*{this.square(2, 6)}*/}
-        {/*</View>*/}
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(3, 0)}*/}
-          {/*{this.square(3, 1)}*/}
-          {/*{this.square(3, 2)}*/}
-          {/*{this.square(3, 3)}*/}
-          {/*{this.square(3, 4)}*/}
-          {/*{this.square(3, 5)}*/}
-          {/*{this.square(3, 6)}*/}
-        {/*</View>*/}
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(4, 0)}*/}
-          {/*{this.square(4, 1)}*/}
-          {/*{this.square(4, 2)}*/}
-          {/*{this.square(4, 3)}*/}
-          {/*{this.square(4, 4)}*/}
-          {/*{this.square(4, 5)}*/}
-          {/*{this.square(4, 6)}*/}
-        {/*</View>*/}
-        {/*<View style={styles.row}>*/}
-          {/*{this.square(5, 0)}*/}
-          {/*{this.square(5, 1)}*/}
-          {/*{this.square(5, 2)}*/}
-          {/*{this.square(5, 3)}*/}
-          {/*{this.square(5, 4)}*/}
-          {/*{this.square(5, 5)}*/}
-          {/*{this.square(5, 6)}*/}
-        {/*</View>*/}
-
-
-
-        <Text style={styles.leaderboard}>
+        <Text style={styles.leaderBoard}>
           Player 1's Score: {this.state.redScore}
         </Text>
-        <Text style={styles.leaderboard}>
+        <Text style={styles.leaderBoard}>
           Player 2's Score: {this.state.yellowScore}
         </Text>
         <Button title="Restart Game" onPress={() => this.reset()}/>
@@ -274,51 +205,51 @@ export default class App extends Component<Props> {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  tile: {
-    borderWidth: 3,
-    width: 50,
-    height: 50,
-    backgroundColor: 'blue',
-  },
-  button: {
-    width: 50,
-    height: 50,
-    color : 'transparent'
-  },
-  header: {
-    fontSize: 55,
-    fontWeight: 'bold',
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    tile: {
+      borderWidth: 3,
+      width: 50,
+      height: 50,
+      backgroundColor: 'blue',
+    },
+    button: {
+      width: 50,
+      height: 50,
+      color: 'transparent'
+    },
+    header: {
+      fontSize: 55,
+      fontWeight: 'bold',
 
 
-  },
-  leaderboard: {
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  yellow: {
-    fontSize: 45,
-    color: 'yellow',
-    textAlign: 'center'
-  },
-  red: {
-    fontSize: 45,
-    color: 'red',
-    textAlign: 'center'
-  },
-  white: {
-    fontSize: 45,
-    color: 'white',
-    textAlign: 'center'
-  },
-  row: {
-    flexDirection: 'row',
-    display: 'flex'
-  }
+    },
+    leaderBoard: {
+      fontSize: 20,
+      fontWeight: 'bold'
+    },
+    yellow: {
+      fontSize: 45,
+      color: 'yellow',
+      textAlign: 'center'
+    },
+    red: {
+      fontSize: 45,
+      color: 'red',
+      textAlign: 'center'
+    },
+    white: {
+      fontSize: 45,
+      color: 'white',
+      textAlign: 'center'
+    },
+    row: {
+      flexDirection: 'row',
+      display: 'flex'
     }
+  }
 );
